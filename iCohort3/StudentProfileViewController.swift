f2be5ff4e2584c029a9376179da24bfabebd5f95
ssshreya24 <shreya131268@gmail.com>
2025-11-06 02:31:33 +0530

    Student Profile

---
+++ iCohort3/StudentProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudentProfileViewController.swift
+//  iCohort3
+//
+//  Created by Shreya on 06/11/25.
+//
+
+import UIKit
+
+class StudentProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
