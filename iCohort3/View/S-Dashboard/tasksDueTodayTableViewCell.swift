b147af082c4872233936d47062a3cd48680fea9a
Shruti Kulashri <kulashrishruti@gmail.com>
2025-11-20 12:17:53 +0530

    poop

---
+++ iCohort3/View/S-Dashboard/tasksDueTodayTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  tasksDueTodayTableViewCell.swift
+//  iCohort3
+//
+//  Created by user@56 on 20/11/25.
+//
+
+import UIKit
+
+class tasksDueTodayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
